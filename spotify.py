f72cab431dcd19d1907060927df9c6a696ba7492
Yan <yan_j1213@126.com>
2017-03-12 05:59:23 +0800

    update tweepy

--- spotify.py
+++ spotify.py
@@ -13,6 +13,20 @@ RANDOM_STATE = ''
 
 app = Flask(__name__)
 
+#----------------------Get info from Twitter through Tweepy----------------------#
+import tweepy
+
+#Create variables for each key, secret, token.
+consumer_key = 'hrABYWoeNcdDgyMYUPqlcITNi'
+consumer_secret = '8KkiJEgeJySQ8ZFDA2ywHPx2cagifHqiXS7jxcsCWDtLPGV59K'
+access_token = '838796084038230016-L8xC5JUjkDxUvfNJR4iZACE5QIKlXMG'
+access_token_secret = 'jhQC3kWgc8SbkmlMnJ5965UDddeV61EbGbqcUnfzI3DuQ'
+
+#Set up OAuth and integrate with API.
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
 # Prompt login
 @app.route('/')
 def homepage():
